5923620d9ad6be50f35583ceaa96ec9b33c2da08
ori <oriyedidlevy@gmail.com>
2018-05-05 17:00:41 +0300

    change

--- ParentPatrol/src/app/app.module.ts
+++ ParentPatrol/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -12,6 +11,7 @@ import { ReitemPage } from '../pages/reitem/reitem';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AddUserPage } from '../pages/add-user/add-user';
+
 import { ReproviderProvider } from '../providers/reprovider/reprovider';
 
 
