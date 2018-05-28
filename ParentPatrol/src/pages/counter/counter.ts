41f7dbe3b0c528f10fd8153cc3229ed8474a89a1
LilachhN <lilachnoyy@gmail.com>
2018-05-28 21:11:51 +0300

    .

--- ParentPatrol/src/pages/counter/counter.ts
+++ ParentPatrol/src/pages/counter/counter.ts
@@ -4,6 +4,8 @@ import { Pedometer } from '@ionic-native/pedometer';
 import { Platform, ModalController } from 'ionic-angular';
 import { SettingsProvider } from '../../providers/settings/settings';
 import { SettingsPage } from '../../pages/settings/settings';
+import { isEmpty } from 'rxjs/operator/isEmpty';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-counter',
@@ -13,8 +15,10 @@ export class CounterPage {
   steps: number = 0;
   goal: number;
   percentage: number;
+  resetNum: number;
  
-  constructor(private ref: ChangeDetectorRef, public platform: Platform, public pedometer: Pedometer, public modalCtrl: ModalController, public settings: SettingsProvider) {
+  constructor(private ref: ChangeDetectorRef, public platform: Platform, public pedometer: Pedometer, public modalCtrl: ModalController, public settings: SettingsProvider,
+  private alertCtrl: AlertController) {
     this.pedometer.startPedometerUpdates()
       .subscribe((data) => {
           this.steps = data.numberOfSteps;
@@ -38,5 +42,27 @@ export class CounterPage {
       }
     })
     modal.present();
+  }
+
+  resetSteps(){
+    let alert = this.alertCtrl.create({
+      title: 'איפוס צעדים',
+      message: 'האם לאפס?',
+      buttons: [
+        {
+          text: 'כן',
+          handler: () => {
+            this.steps=0;
+             this.percentage=0;
+          }
+        },
+        {
+          text: 'לא',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
