643400b2021969b7090540640998107dc8458fdd
LilachhN <lilachnoyy@gmail.com>
2018-06-18 00:50:11 +0300

    download reports data

--- ParentPatrol/src/pages/about/about.ts
+++ ParentPatrol/src/pages/about/about.ts
@@ -28,6 +28,7 @@ export class AboutPage {
   startDate;
   endDate;
   jsonStr="";
+  x=0;
 
   constructor(private platform:Platform,private alertCtrl:AlertController,public navCtrl: NavController,private afDatabase : AngularFirestore,private loading: LoadingController, private lp:LocationsProvider) {
     const firestore = firebase.firestore();
@@ -62,6 +63,15 @@ export class AboutPage {
 
   
 async dataJson(){
+  if(this.startDate==undefined || this.endDate==undefined){
+   let alert = this.alertCtrl.create({
+    title: 'שגיאה',
+    subTitle: 'נא להזין תאריכים',
+    buttons: ['אישור']
+    });
+    alert.present();
+    return
+  }
   this.jsonStr="";
   var a = moment(this.startDate);
   var b = moment(this.endDate);
@@ -79,13 +89,121 @@ async dataJson(){
   }
 
   load.dismiss()
+  this.x=1;
+ 
 
   }
 
-  
+  download(){
+    if(this.jsonStr==""){
+      let alert = this.alertCtrl.create({
+       title: 'שגיאה',
+       subTitle: 'לא קיימים נתונים לתאריכים אלו',
+       buttons: ['אישור']
+       });
+       alert.present();
+       this.x=0;
+       return
+     }
+    let title:string[];
+    title=[ 
+    "Alcohol",
+    "Ambulance",
+    "AverageAge",
+    "Drugs",
+    "EndTime",
+    "Exeptions",
+    "GroupNum",
+    "Handle",
+    "Kind",
+    "MembersInGroup",
+    "MorePlaces",
+    "MyDate",
+    "Notes",
+    "Place",
+    "Police",
+    "StartTime",
+    "Team",
+    "TeamNumbe",
+    "Vandalism",
+    "Violence",
+    "VolenteersName",
+    "underAlcoho",
+    "underDrugs",
+    "Dilemmas",
+    "NumOfYoungsters",
+    "YoungsterNamee",
+    ]
+   // let csvContent = "data:text/csv;charset=utf-8,";
  
-}
+   //let csvContent =Bom+ "data:text/csv;charset=utf-8,";
+  
+   
+    
+    let row = "";
+    let row2= "";
+    let obj:Object
+    let kind;
+    for (let key in title){
+      row += title[key] + ","
+    }
+    let j=0,i=0
+    let data=this.jsonStr.split('%');
+    for(let i=0;i<data.length-1;i++){
+        obj = JSON.parse(data[i]);
+        kind=this.kind(obj);
 
+        if(kind=="hot spot"){
+          for(let key in obj){
+            row2 += obj[key] + ","
+          }
+          row2+='\r\n'
+        }
+
+        if(kind=="cold spot"){
+          
+          row2+=",,,,"+this.getVal(obj,"EndTime")+",,,,"+this.getVal(obj,"Kind")+",,,"+this.getVal(obj,"MyDate")+
+          ","+this.getVal(obj,"Notes")+",,,"+this.getVal(obj,"StartTime")+",,"+this.getVal(obj,"TeamNumbe")+",,,"+this.getVal(obj,"VolenteersName")+",,"
+          row2+='\r\n'
+        }
+
+        
+        if(kind=="moadonit"){
+          row2+=this.getVal(obj,"AlcoholOrDrugs")+",,"+this.getVal(obj,"AverageAge")+",,"+this.getVal(obj,"EndTime")+","+this.getVal(obj,"Exeptions")+",,"+this.getVal(obj,"Handle")+","+this.getVal(obj,"Kind")+",,,"+this.getVal(obj,"MyDate")+","+this.getVal(obj,"Notes")+",,,"+this.getVal(obj,"StartTime")+","+this.getVal(obj,"Team")+","+this.getVal(obj,"TeamNumbe")+",,,"+this.getVal(obj,"VolenteersName")+",,,"+this.getVal(obj,"Dilemmas")+","+this.getVal(obj,"NumOfYoungsters")+","+this.getVal(obj,"YoungsterName")
+          row2+='\r\n'
+        }
+
+        
+       
+    }  
+
+ 
+    let csvContent = row.slice(0, row.length-2) + "\r\n" + row2 + "\n\r"
+    var link = window.document.createElement("a");
+    link.setAttribute("href", "data:text/csv;charset=utf-8,%EF%BB%BF" + encodeURI(csvContent));
+    link.setAttribute("download", this.startDate+"-"+this.endDate+".csv");
+    link.click();
+    this.x=0;
+
+  }
+
+  getVal(obj,string){
+    for(let key in obj){
+      if(key==string)
+      return obj[key]
+    }
+  }
+  kind(obj){
+    for(let key in obj){
+      if(key=="Kind"){
+        return obj[key]
+      }
+    }
+
+  }
+
+
+}
 
 
 
